c17537c7eaa3a94f1aeead42c4e5af3fe24cfb10
prayogorizkymaulany <prayogo0907@gmail.com>
2018-01-03 20:17:02 +0800

    matriks x

--- saw.cpp
+++ saw.cpp
@@ -3,3 +3,10 @@
 using namespace std;
 
 int main() {
+	cout.setf(ios::fixed);
+	cout.setf(ios::showpoint);
+	cout.precision(4);
+	cout << "MATRIKS X : "<<endl;
+	double ac[3][5]= {
+	{4,4,5,3,3}, {3,3,4,2,3}, {5,4,2,2,2}
+};
